e73de5687a2ddbaa23ccaea4b88cf68bff939bfb
Holly-Ellington <146004914+Holly-Ellington@users.noreply.github.com>
2023-12-27 11:34:54 -0600

    Add files via upload

---
+++ src/artists/insert-multiple-artists.sql
@@ -0,0 +1,5 @@
+INSERT INTO artists (first_name, last_name, birthday, is_alive)
+VALUES 
+  ('Cher', 'Bono', '1950-02-28', true),
+  ('Sony', 'Bono', '1941-02-22', true),
+  ('Britany', 'Spears', '1977-02-28', true);
